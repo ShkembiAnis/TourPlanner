2ff86a0f32b2cacbb9f5843787ff1c237e0c2d5c
Anis Shkembi <anis.shkembi@gmail.com>
2022-03-12 14:05:21 +0100

    correction try 2

---
+++ src/main/java/at/technikum_wien/tourplanner_anis_mariel/HelloController.java
@@ -0,0 +1,14 @@
+package at.technikum_wien.tourplanner_anis_mariel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module at.technikum_wien.tourplanner_anis_mariel {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens at.technikum_wien.tourplanner_anis_mariel to javafx.fxml;
+    exports at.technikum_wien.tourplanner_anis_mariel;
+}
