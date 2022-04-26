a34f7f92ab3e699e2f481f0edeec1bfd1ad6382d
Anis Shkembi <anis.shkembi@gmail.com>
2022-04-26 09:12:49 +0200

    improving on list

--- src/main/java/at/technikum_wien/tourplanner_anis_mariel/MainViewController.java
+++ src/main/java/at/technikum_wien/tourplanner_anis_mariel/MainViewController.java
@@ -1,14 +1,33 @@
 package at.technikum_wien.tourplanner_anis_mariel;
 
+import at.technikum_wien.tourplanner_anis_mariel.tourAdd.TourAddController;
+import at.technikum_wien.tourplanner_anis_mariel.tourAdd.TourListModel;
+import at.technikum_wien.tourplanner_anis_mariel.tourAdd.TourListingController;
+import at.technikum_wien.tourplanner_anis_mariel.tourAdd.TourModel;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class MainViewController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+    private TourListModel tourListModel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TourAddController tourAddController;
+
+    @FXML
+    private TourListingController tourListingController;
+
+    public MainViewController(TourListModel tourListModel) {
+        this.tourListModel = tourListModel;
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.tourAddController.addListener(
+                p -> this.tourListModel.addProduct(TourModel.From(p)));
+    }
+
 }
--- src/main/java/at/technikum_wien/tourplanner_anis_mariel/tourAdd/ControllerFactory.java
+++ src/main/java/at/technikum_wien/tourplanner_anis_mariel/tourAdd/ControllerFactory.java
@@ -1,5 +1,7 @@
 package at.technikum_wien.tourplanner_anis_mariel.tourAdd;
 
+import at.technikum_wien.tourplanner_anis_mariel.MainViewController;
+
 public class ControllerFactory {
     private final TourListModel tourListModel;
     private final TourModel tourModel;
@@ -14,8 +16,8 @@ public class ControllerFactory {
     public Object create(Class controllerClass) throws Exception {
         if (controllerClass == TourAddController.class) {
             return new TourAddController(this.tourAddModel);
-        } else if (controllerClass == TourListController.class) {
-            return new TourListController(this.tourListModel);
+        } else if (controllerClass == MainViewController.class) {
+            return new MainViewController(this.tourListModel);
         } else if (controllerClass == TourListingController.class) {
             return new TourListingController(this.tourListModel);
         }else {
